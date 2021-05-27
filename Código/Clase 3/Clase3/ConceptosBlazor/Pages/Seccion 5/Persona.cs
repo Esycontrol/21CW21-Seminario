e985c775111a2719abdffb5e7d5855e39c6499c3
Esycontrol <pgonzalez@esycontrol.com>
2021-05-27 19:03:30 +0200

    Clase 4

---
+++ Código/Clase 3/Clase3/ConceptosBlazor/Pages/Seccion 5/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ConceptosBlazor.Pages.Seccion_5
+{
+    public class Persona
+    {
+        [Required]
+        public string Nombre { get; set; }
+        [Range(0,Int32.MaxValue)]
+        public int Edad { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
